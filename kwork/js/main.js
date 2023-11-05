fc1369c74623b7798add1d5ca0e2803b83ffda0e
budu2906 <101338515+budu2906@users.noreply.github.com>
2023-11-05 12:58:13 +0400

    Add files via upload

---
+++ kwork/js/main.js
@@ -0,0 +1,20 @@
+// const newsBtn = document.getElementById('news-button')
+// const news = document.getElementById('responsive-news')
+// let toggleNews = false
+
+// const newsFn = () =>{
+//     newsBtn.addEventListener('click', () =>{
+//         toggleNews=!toggleNews
+//         if(toggleNews){
+//             news.style.display= 'flex'
+//             newsBtn.textContent='Скрыть'
+//         }
+//         else{
+//             news.style.display ='none'
+//             newsBtn.textContent='ещё'
+//         }
+//     })
+
+   
+// }
+// newsFn()
